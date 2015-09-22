5a27aee622f6dbefa76a143f1f1c848328617129
bkorotaev <54k@bk.ru>
2015-09-23 01:22:07 +0500

    Removed unused packages

--- src/main/java/io/lighty/network/ChannelFutureGroupListener.java
+++
@@ -1,6 +0,0 @@
-package io.lighty.network;
-
-import io.lighty.network.concurrent.FutureListener;
-
-public interface ChannelFutureGroupListener extends FutureListener<ChannelFutureGroup> {
-}
--- src/main/java/io/lighty/network/util/ObjectMatcher.java
+++
@@ -1,6 +0,0 @@
-package io.lighty.network.util;
-
-public interface ObjectMatcher<T> {
-
-    boolean match(T object);
-}
